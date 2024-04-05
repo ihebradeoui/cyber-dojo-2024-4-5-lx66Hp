57e402cb75a09fa422ade217e83621b9ee40fe4a
lx66Hp <lx66Hp@cyber-dojo.org>
2024-04-05 10:20:56 +0000

    2 ran tests, no prediction, got amber

--- files/HikerTest.java
+++ files/HikerTest.java
@@ -10,8 +10,19 @@ public class HikerTest {
 
     @Test
     void life_the_universe_and_everything() {
-        int expected = 42;
-        int actual = Hiker.answer();
-        assertEquals(expected, actual);
-    }
+        List<String> actual = Hiker.answer().split("\n");
+        for (int i =0 ; i<actual.size(); i++)
+        {
+            int currentNumber=i+1
+            if(currentNumber%3==0 && currentNumber%5==0)
+                assertEquals("FizzBuzz", actual[i]);
+            else if(currentNumber%3==0)
+                assertEquals("Buzz", actual[i]);
+            else if(currentNumber%3==0)
+                assertEquals("Buzz", actual[i]);
+            else 
+                assertEquals(currentNumber.toString(), actual[i]);
+        }
+        
+     }
 }
